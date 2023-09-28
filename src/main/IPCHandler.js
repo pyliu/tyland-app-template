736e9a11fbd4bc100d42310af97a733bb7880c9e
pyliu <3634168+pyliu@users.noreply.github.com>
2023-09-28 22:38:12 +0800

    add template method

--- src/main/IPCHandler.js
+++ src/main/IPCHandler.js
@@ -1,4 +1,4 @@
-const { ipcMain } = require('electron')
+const { ipcMain, app } = require('electron')
 
 export default class IPCHandler {
   constructor () {
@@ -9,8 +9,20 @@ export default class IPCHandler {
     // this method is a dispatcher
     const type = payload.type
     switch (type) {
+      case 'version':
+        this.handleVersion(payload)
       default:
         console.warn(`${type} not supported!`)
     }
+  }
+  /**
+   * handler impl
+   */
+  handleVersion (payload) {
+    ipcMain.handle('version', async (event, payload) => {
+      const version = app.getVersion()
+      console.log(`APP version is`, version)
+      return version
+    })
   }
 }
