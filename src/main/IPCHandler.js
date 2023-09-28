4186a714b984189c0c79856f3dd4ac2998d35da6
pyliu <3634168+pyliu@users.noreply.github.com>
2023-09-28 22:50:20 +0800

    add 'open-browser' type

--- src/main/IPCHandler.js
+++ src/main/IPCHandler.js
@@ -9,8 +9,12 @@ export default class IPCHandler {
     // this method is a dispatcher
     const type = payload.type
     switch (type) {
+      case 'open-browser':
+        this.handleOpenBrowser(payload)
+        break
       case 'version':
         this.handleVersion(payload)
+        break
       default:
         console.warn(`${type} not supported!`)
     }
@@ -23,6 +27,14 @@ export default class IPCHandler {
       const version = app.getVersion()
       console.log(`APP version is`, version)
       return version
+    })
+  }
+
+  handleOpenBrowser (payloaad) {
+    ipcMain.handle('open-explorer', async (event, payload) => {
+      const url = payload.url || payload.path || payload
+      // use default browser to open the url
+      require('child_process').exec(`start "" "${url}"`)
     })
   }
 }
