23d908fb14a95ba74ca704fe86344c314268c02f
pyliu <3634168+pyliu@users.noreply.github.com>
2023-10-03 16:45:21 +0800

    add basic ipc handler

--- src/main/IPCHandler.js
+++ src/main/IPCHandler.js
@@ -2,39 +2,37 @@ const { ipcMain, app } = require('electron')
 
 export default class IPCHandler {
   constructor () {
-    ipcMain.handle('invoke', this._handleInvoking)
+    ipcMain.handle('command', this._handleCommand.bind(this))
   }
 
-  async _handleInvoking (payload) {
+  async _handleCommand (event, payload) {
+    console.olog(payload)
     // this method is a dispatcher
     const type = payload.type
     switch (type) {
       case 'open-browser':
-        this.handleOpenBrowser(payload)
-        break
+        return this.handleOpenBrowser(payload)
       case 'version':
-        this.handleVersion(payload)
-        break
+        return this.handleVersion(payload)
       default:
         console.warn(`${type} not supported!`)
+        return `${type} not supported!`
     }
   }
   /**
    * handler impl
    */
   handleVersion (payload) {
-    ipcMain.handle('version', async (event, payload) => {
-      const version = app.getVersion()
-      console.log(`APP version is`, version)
-      return version
-    })
+    const version = app.getVersion()
+    console.log(`APP version is ${version}`)
+    return version
   }
 
-  handleOpenBrowser (payloaad) {
-    ipcMain.handle('open-explorer', async (event, payload) => {
-      const url = payload.url || payload.path || payload
-      // use default browser to open the url
-      require('child_process').exec(`start "" "${url}"`)
-    })
+  handleOpenBrowser (payload) {
+    const url = payload.url || payload.path || payload
+    console.log(`exec => start "" "${url}"`)
+    // use default browser to open the url
+    require('child_process').exec(`start "" "${url}"`)
+    return true
   }
 }
